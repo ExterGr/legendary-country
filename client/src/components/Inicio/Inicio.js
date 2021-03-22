7c75ae120550b04c18ee1d9724190635f36e053a
ExterGr <ivankudacki@gmail.com>
2021-03-21 23:13:37 -0300

    home and country css 2

--- client/src/components/Inicio/Inicio.js
+++ client/src/components/Inicio/Inicio.js
@@ -9,10 +9,15 @@ function Inicio(props) {
         <div>
             <a href="/home" className="initial-button" onClick={()=> props.getCountries()}>Iniciar</a>
             <h1 className="landing-title">Henry countries</h1>
-            <img src="https://img2.pngio.com/icono-mundo-mostrar-americas-gratis-d-126983-png-images-pngio-mundo-png-512_512.png" className="landing-image" alt=""/>
+            <img src="https://acegif.com/wp-content/uploads/Earth.gif" className="landing-image" alt=""/>
             <i className="landing-sign">By Ivan Kudacki</i>
         </div>
     );
 }
 
 export default Inicio;
+
+//https://img2.pngio.com/icono-mundo-mostrar-americas-gratis-d-126983-png-images-pngio-mundo-png-512_512.png
+//https://ftsamuelrobinson.files.wordpress.com/2015/02/planeta-gif-2b924d2.gif
+//https://im7.ezgif.com/tmp/ezgif-7-d84d033fc8e7.gif
+//https://acegif.com/wp-content/uploads/Earth.gif
